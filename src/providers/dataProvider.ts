6cfff55e136229f72c8eb8a704c63cb698517cfb
Henrique Cardoso <henrique@oniweb.com.br>
2025-12-23 21:31:01 +0100

    feat: config to use VITE_API_URL env var

--- src/providers/dataProvider.ts
+++ src/providers/dataProvider.ts
@@ -1,6 +1,6 @@
 import simpleRestProvider from 'ra-data-simple-rest';
 import { httpClient } from '../utils/httpClient';
 
-const API_URL = 'http://localhost:8080/v1';
+const API_URL = import.meta.env.VITE_API_URL || 'http://localhost:8080/v1';
 
 export const dataProvider = simpleRestProvider(API_URL, httpClient);
